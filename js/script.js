5bff405d591b2321130d9958c6cb96849d9fda8d
Sicily <50249336+wt163@users.noreply.github.com>
2026-09-04 21:02:00 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ function initializeFooterInteractions() {
         // Handle link targets
         const href = link.getAttribute('href');
         
-        if (href && href.startsWith('https://ytclipmind.com/')) {
+        if (href && (href.startsWith('https://ytclipmind.com/') || href.startsWith('/Terms_of_service') || href.startsWith('/Privacy_policy') || href.startsWith('/Refund_policy'))) {
             // Legal page links - open in new tab
             link.setAttribute('target', '_blank');
             link.setAttribute('rel', 'noopener noreferrer');
